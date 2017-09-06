c1741ee624c155105cf6a7a77076c0c4de40c7bf
xi liu <1157402256@qq.com>
2017-09-07 01:31:59 +0800

    添加TestIterable.py
    TestIterable.py演示了如何用Iterable判断一个对象是否是可迭代对象。

---
+++ TestItreable.py
@@ -0,0 +1,13 @@
+"""
+利用collections模块的Iterable类型判断来判断一个对象是否是可迭代对象
+"""
+from collections import Iterable
+
+print(isinstance([],Iterable))
+print(isinstance((),Iterable))
+print(isinstance("",Iterable))
+print(isinstance(range(10),Iterable))
+print(isinstance({},Iterable))
+print(isinstance({}.keys(),Iterable))
+print(isinstance({}.values(),Iterable))
+print(isinstance({}.items(),Iterable))
