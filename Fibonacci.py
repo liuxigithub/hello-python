db7301a846d10365bbacbb583ff0849751d51386
xi liu <1157402256@qq.com>
2018-05-08 16:19:12 +0800

    2018-5-8

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 (a, b)= (0, 1)
-while b < 10:
-  print(b)
-  (a, b) = (b, a+b)
-  
+i = 0
+while i < 50:
+    print(b)
+    (a, b) = (b, a+b)
+    i = i+1
 #input()
   
-print('name: {}, age: {}'.format('Liu xi',27))
+print('name: {}, age: {}'.format('Liu xi',28))
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # hello-world
 Learning Python
 学习Python
+
+专题式的笔记，而不是备课本式的笔记
