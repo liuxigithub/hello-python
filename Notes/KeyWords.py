8a6f412913a04aaeff5b65a0500ac368916493e0
xi liu <1157402256@qq.com>
2018-05-24 16:30:18 +0800

    24

---
+++ Notes/KeyWords.py
@@ -0,0 +1,6 @@
+"""
+Python关键字介绍
+ref: https://blog.csdn.net/s1h2e3n4g5/article/details/76416257
+     https://blog.csdn.net/liang19890820/article/details/68488392
+     以后再写
+"""
---
+++ Notes/OperatorPriority.py
@@ -0,0 +1,5 @@
+"""
+Python运算符优先级
+ref: https://www.sojson.com/operation/python.html
+以后再写
+"""
